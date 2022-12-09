46827237f971ac1eaa6108202163e3673f72f7ea
Edidiong Obodom <edijay17@gmail.com>
2022-12-09 11:00:58 +0100

    modified:   src/index.css

--- src/components/UI/Background_image/ImgBack.js
+++ src/components/UI/Background_image/ImgBack.js
@@ -1,28 +1,42 @@
 import classes from "./ImgBack.module.css";
+import { useEffect, useCallback } from "react";
+import { useState } from "react";
 
 const ImgBack = () => {
+  
+  const names = ['Safe and secure banking service', 'Banking made easy and swift']
+  const [newName, setnewName] = useState("");
+
+  const shuffle = useCallback(() => {
+      const index = Math.floor(Math.random() * names.length);
+      setnewName(names[index]);
+  }, [names]);
+
+  useEffect(() => {
+      const intervalID = setInterval(shuffle, 4000);
+      return () => clearInterval(intervalID);
+  }, [shuffle])
+
   return (
-    <div id="caro" class={`carousel slide carousel-fade ${classes.img0}`} data-bs-ride="carousel">
-  <div class={`carousel-inner ${classes.img0}`}>
-    <div className={`carousel-item active ${classes.img}`}>
+  //   <div id="caro" class={`carousel slide carousel-fade ${classes.img0}`} data-bs-ride="carousel">
+  // <div class={`carousel-inner ${classes.img0}`}>
+  <>
+    <div className={` ${classes.img}`}>
       <span>
-        <h1 className={`${classes.imgText} ${classes.big}`}>Banking made easy and swift</h1>
+        <h1 className={`${classes.imgText} ${classes.big}`}>
+          {newName === "" ? names[1] : newName}
+          
+          </h1>
         {/* <p className={`${classes.imgText} ${classes.small}`}>
           Using cutting edge technology for swift and stress free transactions
           </p> */}
       </span>
       
     </div>
-    <div className={`carousel-item ${classes.img1}`} >
-      <span>
-        <h1 className={`${classes.imgText} ${classes.big}`}>Safe and secure banking service</h1>
-        {/* <p className={`${classes.imgText} ${classes.small}`}>
-          Using cutting edge technology for security of client's funds
-          </p> */}
-      </span>
-    </div>
-    </div>
-  </div>
+    
+    </>
+  //   </div>
+  // </div>
   
   );
 };
