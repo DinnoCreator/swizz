735a079e98408a1eb12a34b4f5650d005e98d374
DinnoCreator <97753291+DinnoCreator@users.noreply.github.com>
2022-12-12 09:58:16 +0100

    Update Perk.js

--- src/components/UI/Perks/Perk.js
+++ src/components/UI/Perks/Perk.js
@@ -7,21 +7,21 @@ function Perk() {
       <div className="row">
         <h1>What do we offer?</h1>
         <div className={`${classes.featureBox} col-lg-4`}>
-          <Link to="/about-us">
+          <Link to="/about">
             <i className={`fa-solid fa-lock ${classes.icon}`}></i>
           </Link>
           <h3 className={classes.featuresHeading}>Secure Banking</h3>
           <p>Your data and funds would be securely protected.</p>
         </div>
         <div className={`${classes.featureBox} col-lg-4`}>
-          <Link to="/about-us">
+          <Link to="/about">
             <i className={`fa-solid fa-chart-pie ${classes.icon}`}></i>
           </Link>
           <h3 className={classes.featuresHeading}>Investment</h3>
           <p>We offer you the best and safest investment options.</p>
         </div>
         <div className={`${classes.featureBox} col-lg-4`}>
-          <Link to="/about-us">
+          <Link to="/about">
             <i className={`fa-solid fa-boxes-stacked ${classes.icon}`}></i>
           </Link>
           <h3 className={classes.featuresHeading}>Multiple Methods</h3>
